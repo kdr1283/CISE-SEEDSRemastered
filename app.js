a7541c373d769e1bd82dada59e94ab314f86e0a2
Rafael <ramos.rafael42@gmail.com>
2021-05-16 14:24:47 +1200

    Checks in app.js whether the node_env is production or development

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config({ path: "./config.env"});
+const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db');
 const articles = require("./routes/api/articles");
@@ -13,9 +14,19 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
-
 app.use('/api/articles', articles);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send("Api running");
+    })
+}
 
 const port = process.env.PORT || 8082;
 
