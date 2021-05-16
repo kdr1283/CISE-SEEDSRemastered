75fe3f52f62d95585f3b760a8597551fa8d34634
Rafael <ramos.rafael42@gmail.com>
2021-05-16 14:20:05 +1200

    Update app.js
    articles api connection
    cors
    init middleware

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 require('dotenv').config({ path: "./config.env"});
 const express = require('express');
 const connectDB = require('./config/db');
+const articles = require("./routes/api/articles");
 
 //Connect database
 connectDB();
 const app = express();
 
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('Hello world!'));
+
+app.use('/api/articles', articles);
 
 const port = process.env.PORT || 8082;
 
