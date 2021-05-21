d6984bdc64d0b1981bc4ef2d07b9ce1744057365
Rafael <68944880+kdr1283@users.noreply.github.com>
2021-05-21 12:51:15 +1200

    Update App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,10 @@
 // import { render, screen } from '@testing-library/react';
-// import App from './App';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
+import axios from 'axios';
+import ShowArticleList from './components/ShowArticleList';
+import ArticleTable from './components/ArticleTable';
 
 // test('renders learn react link', () => {
 //   render(<App />);
@@ -8,7 +13,33 @@
 // });
 
 describe("Addition", () => {
-  it("knows that 2 and 2 make 4", () => {
+  it("knows that 3 and 3 make 6", () => {
     expect(3 + 3).toBe(6)
   })
 });
+
+
+it('renders App without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders ShowArticleList without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ShowArticleList />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('api exists', () => {
+  axios
+      .get("https://team-7-seeds.herokuapp.com/api/articles")
+      .then((res) => {
+        this.setState({
+          articles: res.data,
+        })
+      })
+      .catch((err) => {
+        console.log("Error from ShowArticleList")
+      })
+})
