6ea03007d5f6a88dd93a5d104eda01d6efba91c8
Rafael <ramos.rafael42@gmail.com>
2021-05-23 13:09:50 +1200

    eslint and prettier with custom config

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -9,6 +9,6 @@
 
 describe("Addition", () => {
   it("knows that 2 and 2 make 4", () => {
-    expect(3 + 3).toBe(6)
-  })
+    expect(3 + 3).toBe(6);
+  });
 });
--- client/src/components/ArticleTable.js
+++ client/src/components/ArticleTable.js
@@ -1,53 +1,51 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-array-index-key */
+
+import React from "react";
 import "../index.css";
 
 export default class ArticleTable extends React.Component {
-    
-    constructor(props){
-      super(props);
-      this.getColumns = this.getColumns.bind(this);
-      this.getRows = this.getRows.bind(this);
-      this.getKeys = this.getKeys.bind(this);
-    }
-    
-    getKeys = function() {
-      return Object.keys(this.props.data[0]);
-    }
-    
-    getColumns = function() {
-      var keys = this.getKeys();
-      return keys.map((key, index)=>{
-        return <th key={key}>{key.toUpperCase()}</th>
-      })
-    }
-    
-    getRows = function() {
-      var records = this.props.data;
-      var keys = this.getKeys();
-      return records.map((row, index)=>{
-        return <tr key={index}><RenderRow key={index} data={row} keys={keys}/></tr>
-      })
-    }
-    
-    render() {
-        return (
-          <div>
-            <table id='articles'>
-            <thead>
-              <tr>{this.getColumns()}</tr>
-            </thead>
-            <tbody>
-              {this.getRows()}
-            </tbody>
-            </table>
-          </div>
-          
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.getColumns = this.getColumns.bind(this);
+    this.getRows = this.getRows.bind(this);
+    this.getKeys = this.getKeys.bind(this);
+  }
+
+  getKeys = function () {
+    return Object.keys(this.props.data[0]);
+  };
+
+  getColumns = function () {
+    const keys = this.getKeys();
+    return keys.map((key, index) => <th key={key}>{key.toUpperCase()}</th>);
+  };
+
+  getRows = function () {
+    const records = this.props.data;
+    const keys = this.getKeys();
+    return records.map((row, index) => (
+      <tr key={index}>
+        <RenderRow key={index} data={row} keys={keys} />
+      </tr>
+    ));
+  };
+
+  render() {
+    return (
+      <div>
+        <table id="articles">
+          <thead>
+            <tr>{this.getColumns()}</tr>
+          </thead>
+          <tbody>{this.getRows()}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
-const RenderRow = (props) => {
-  return props.keys.map((key, index)=>{
-    return <td key={props.data[key]}>{props.data[key]}</td>
-  })
-}
+const RenderRow = (props) =>
+  props.keys.map((key, index) => (
+    <td key={props.data[key]}>{props.data[key]}</td>
+  ));
